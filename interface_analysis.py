1678038644497d3c79f692d0b2b617341bc223eb
orrbeer <orrbeer@gmail.com>
2024-12-08 15:41:52 +0200

    Manage to fix one problem but messed up with the other

--- interface_analysis.py
+++ interface_analysis.py
@@ -66,89 +66,69 @@ files = ['images/br.tif', 'images/90_rt_cl_15-1.tif']
 
 
 
-roi_x1, roi_x2, roi_y1, roi_y2 = cl_roi_x1, cl_roi_x2,cl_roi_y1,cl_roi_y2
-zoom_x1, zoom_x2, zoom_y1, zoom_y2 = cl_zoom_x1, cl_zoom_x2, cl_zoom_y1, cl_zoom_y2
-im = plt.imread(files[1])
-s = 2
-rect2 = patches.Rectangle((1200, 960), 238, 12, linewidth=1, edgecolor='w', facecolor='w')
-axis = [0,1500,1030,30]
-text_xy = [1150, 930]
-pix2nm = 238*500
-
-tr = set_ROI(im, roi_y1, roi_y2,roi_x1, roi_x2) # trim the figures for a single interface
-bin = binary_mask(tr)
-edge = skimage.feature.canny(tr, sigma=s) # canny filter that marks the interface
-x, y = edge_image2scatter(edge)
-image, zi = zoom_in(im, zoom_x1, zoom_x2, zoom_y1, zoom_y2)
-roi_zoom_offset = roi_y1-zoom_y1
-print(roi_zoom_offset)
-plt.imshow(tr)
-plt.plot(x,y,'k--')
-
-
-# fig = plt.figure(figsize=(7,5.5))
-# for i,file in enumerate(files):
-    # if i == 0:
-        # roi_x1, roi_x2, roi_y1, roi_y2 = br_roi_x1, br_roi_x2,br_roi_y1,br_roi_y2
-        # zoom_x1, zoom_x2, zoom_y1, zoom_y2 = br_zoom_x1, br_zoom_x2, br_zoom_y1, br_zoom_y2
-        # s = 1.65
-        # rect2 = patches.Rectangle((1200, 960), 238, 12, linewidth=1, edgecolor='w', facecolor='w')
-        # axis = [0,1500,1030,30]
-        # text_xy = [1150, 930]
-        # pix2nm = 238*500
-    # else:
-        # roi_x1, roi_x2, roi_y1, roi_y2 = cl_roi_x1, cl_roi_x2,cl_roi_y1,cl_roi_y2
-        # zoom_x1, zoom_x2, zoom_y1, zoom_y2 = cl_zoom_x1, cl_zoom_x2, cl_zoom_y1, cl_zoom_y2
-        # s = 2
-        # rect2 = patches.Rectangle((2050, 1520), 480, 12, linewidth=1, edgecolor='w', facecolor='w')
-        # axis = [782,2618,1610,390]
-        # text_xy = [2065, 1490]
-        # pix2nm = 480*500
+fig = plt.figure(figsize=(7,5.5))
+for i,file in enumerate(files):
+    if i == 0:
+        roi_x1, roi_x2, roi_y1, roi_y2 = br_roi_x1, br_roi_x2,br_roi_y1,br_roi_y2
+        zoom_x1, zoom_x2, zoom_y1, zoom_y2 = br_zoom_x1, br_zoom_x2, br_zoom_y1, br_zoom_y2
+        s = 2
+        rect2 = patches.Rectangle((1200, 960), 238, 12, linewidth=1, edgecolor='w', facecolor='w')
+        axis = [0,1500,1030,30]
+        text_xy = [1150, 930]
+        pix2nm = 238*500
+    else:
+        roi_x1, roi_x2, roi_y1, roi_y2 = cl_roi_x1, cl_roi_x2,cl_roi_y1,cl_roi_y2
+        zoom_x1, zoom_x2, zoom_y1, zoom_y2 = cl_zoom_x1, cl_zoom_x2, cl_zoom_y1, cl_zoom_y2
+        s = 2
+        rect2 = patches.Rectangle((2050, 1520), 480, 12, linewidth=1, edgecolor='w', facecolor='w')
+        axis = [782,2618,1610,390]
+        text_xy = [2065, 1490]
+        pix2nm = 480*500
     
-    # im = plt.imread(file) #  the scale bar is accross the 1464-1226= 238 pixels 500 nm
-    # tr = set_ROI(im, roi_y1, roi_y2,roi_x1, roi_x2) # trim the figures for a single interface
-    # bin = binary_mask(tr)
-    # edge = skimage.feature.canny(tr, sigma=s) # canny filter that marks the interface
-    # x, y = edge_image2scatter(edge)
-    # image, zi = zoom_in(im, zoom_x1, zoom_x2, zoom_y1, zoom_y2)
+    im = plt.imread(file) #  the scale bar is accross the 1464-1226= 238 pixels 500 nm
+    tr = set_ROI(im, roi_y1, roi_y2,roi_x1, roi_x2) # trim the figures for a single interface
+    bin = binary_mask(tr)
+    edge = skimage.feature.canny(bin, sigma=s) # canny filter that marks the interface
+    x, y = edge_image2scatter(edge)
+    image, zi = zoom_in(im, zoom_x1, zoom_x2, zoom_y1, zoom_y2)
 
 
-    # ax1 = fig.add_subplot(4,2,(1+i,3+i))
+    ax1 = fig.add_subplot(4,2,(1+i,3+i))
 
-    # ax1.imshow(image, aspect="auto")
-    # ax1.add_patch(rect2)
-    # ax1.text(text_xy[0],text_xy[1], '500 nm',color='w')
-    # ax1.axis(axis)
-    # ax1.tick_params(left = False, right = False , labelleft = False , 
-                    # labelbottom = False, bottom = False) 
+    ax1.imshow(image, aspect="auto")
+    ax1.add_patch(rect2)
+    ax1.text(text_xy[0],text_xy[1], '500 nm',color='w')
+    ax1.axis(axis)
+    ax1.tick_params(left = False, right = False , labelleft = False , 
+                    labelbottom = False, bottom = False) 
 
-    # ax2 = fig.add_subplot(4,2,5+i)
+    ax2 = fig.add_subplot(4,2,5+i)
     
-    # ax2.imshow(zi, cmap='gray', aspect="auto")
-    # fit = np.polyfit(x,y,1)
-    # roi_zoom_offset = roi_y1-zoom_y1
-    # ax2.plot([0,(zoom_x2-zoom_x1-1)],
-    # [fit[0]*(zoom_x1)+fit[1]+roi_zoom_offset,fit[0]*(zoom_x2-1)+fit[1]+roi_zoom_offset],'m--',linewidth=1) # Drawing a straight line from two points x = 0 f(0) and x=3000 f(3000)
-    # ax2.plot(x[0:zoom_x2-zoom_x1-1],y[zoom_x1:(zoom_x2-1)]+roi_zoom_offset,'b--',linewidth=1)
+    ax2.imshow(zi, cmap='gray', aspect="auto")
+    fit = np.polyfit(x,y,1)
+    roi_zoom_offset = roi_y1-zoom_y1
+    ax2.plot([0,(zoom_x2-zoom_x1-1)],
+    [fit[0]*(zoom_x1)+fit[1]+roi_zoom_offset,fit[0]*(zoom_x2-1)+fit[1]+roi_zoom_offset],'m--',linewidth=1) # Drawing a straight line from two points x = 0 f(0) and x=3000 f(3000)
+    ax2.plot(x[0:zoom_x2-zoom_x1-1],y[zoom_x1:(zoom_x2-1)]+roi_zoom_offset,'b--',linewidth=1)
 
-    # ax2.tick_params(left = False, right = False , labelleft = False , 
-                    # labelbottom = False, bottom = False) 
+    ax2.tick_params(left = False, right = False , labelleft = False , 
+                    labelbottom = False, bottom = False) 
     
-    # ax3 = fig.add_subplot(4,2,7+i)    
-    # yfit = np.polyval(fit,x)
-    # print(np.shape(yfit))
-    # ax3.plot(x/pix2nm,(yfit-y)/pix2nm)
-    # ax3.set_xlabel('Distance (nm)')
-    # ax3.set_ylabel('Residual (nm)')
+    ax3 = fig.add_subplot(4,2,7+i)    
+    yfit = np.polyval(fit,x)
+    print(np.shape(yfit))
+    ax3.plot(x/pix2nm,(yfit-y)/pix2nm)
+    ax3.set_xlabel('Distance (nm)')
+    ax3.set_ylabel('Residual (nm)')
     # ax3.set_xlim([0, max(x/pix2nm)])
     # ax3.set_ylim([max(yfit-y)*1.2, min(yfit-y)*1.2])
-    # ax3.vlines(x=[zoom_x1/pix2nm, zoom_x2/pix2nm], ymin=min(yfit-y)/pix2nm*1.5, ymax=max(yfit-y)/pix2nm*1.5, colors='k', ls='--', linewidth=0.7)
-    # if i == 1:
-        # ax3.yaxis.set_ticklabels([])
-        # ax3.set_ylabel('')
-    # gof = sum(map(abs, yfit-y))/len(yfit) # The goodness of fit is calculated by the sum of absolute value of errors over the number of pixels
-    # print(gof/pix2nm)
+    ax3.vlines(x=[zoom_x1/pix2nm, zoom_x2/pix2nm], ymin=min(yfit-y)/pix2nm*1.5, ymax=max(yfit-y)/pix2nm*1.5, colors='k', ls='--', linewidth=0.7)
+    if i == 1:
+        ax3.yaxis.set_ticklabels([])
+        ax3.set_ylabel('')
+    gof = sum(map(abs, yfit-y))/len(yfit) # The goodness of fit is calculated by the sum of absolute value of errors over the number of pixels
+    print(gof/pix2nm)
 
-# fig.subplots_adjust(hspace=0.5, right=0.95, top=0.96)
-# plt.savefig(r'Output\example.svg')
+fig.subplots_adjust(hspace=0.5, right=0.95, top=0.96)
+plt.savefig(r'Output\example.svg')
 plt.show()
